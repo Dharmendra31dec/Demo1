1ef3e62a5ff16f953e4f094498634581a722917e
Dharmendra Singh <kumardsingh089@gmail.com>
2026-09-08 02:00:47 +0530

    changed

--- Demo1/src/com/example/Demo.java
+++ Demo1/src/com/example/Demo.java
@@ -2,7 +2,8 @@ package com.example;
 
 public class Demo {
     static void main(String[] args) {
-		System.out.println("Hello-World!");
+		System.out.println("Hello-World1!");
+		System.out.println("HelloWorld!");
 		System.out.println("Hello:welcome");
     }
 }
