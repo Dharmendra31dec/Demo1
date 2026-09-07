2894e4687cfded438a4fee78fe5372954ac8ac59
Dharmendra Singh <kumardsingh089@gmail.com>
2026-09-08 02:10:45 +0530

    Save changes in Demo.java

--- Demo1/src/com/example/Demo.java
+++ Demo1/src/com/example/Demo.java
@@ -2,7 +2,9 @@ package com.example;
 
 public class Demo {
     static void main(String[] args) {
-        System.out.println("Hello-welcome!");
+        System.out.println("Hello-Worlds!");
+		System.out.println("Hello welcome");
+		System.out.println("Hello welcome");
 		System.out.println("Hello welcome");
     }
 }
