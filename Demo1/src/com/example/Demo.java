f4306f2c1564fbab1081f7a41c12ce6baf124418
Dharmendra Singh <kumardsingh089@gmail.com>
2026-09-08 02:19:57 +0530

    added

--- Demo1/src/com/example/Demo.java
+++ Demo1/src/com/example/Demo.java
@@ -2,9 +2,9 @@ package com.example;
 
 public class Demo {
     static void main(String[] args) {
-        System.out.println("Hello-Worlds!");
+        System.out.println("Hello-Woraalds!");
 		System.out.println("Hello welcome");
-		System.out.println("Hello welcome");
+		System.out.println("Hello world");
 		System.out.println("Hello welcome");
     }
 }
